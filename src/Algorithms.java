7fb5a4935c9c81edb3c6520bb2f3c57d4dc30a64
antho <acast453@fiu.edu>
2021-11-30 18:01:19 -0500

    .

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -98,6 +98,32 @@ public class Algorithms
 
         return output;
     }
+
+
+    /**
+     * Given two simple polygons, determines whether they interact.
+     *
+
+     */
+public static boolean isThereAnIntersection(SimplePolygon pol1, SimplePolygon pol2){
+LineSegment [] edges1 = pol1.getEdges();
+LineSegment [] edges2 = pol2.getEdges();
+
+for(int i =0; i < pol1.getNumberOfVertices();i++)
+{
+    LineSegment e1 = edges1[i];
+
+    for (int j =0; j < pol1.getNumberOfVertices(); j++)
+    {
+        LineSegment e2 = edges2[j];
+        Point p = new Point();
+        if (e1.intersect(e2,p) ==1) return true;
+    }
+}
+
+
+    return false;
+}
 }
 
 class PointComparator implements Comparator<Point>
